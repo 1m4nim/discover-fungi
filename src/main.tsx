6bb39cfe424bd80784fc89b58b525a162d7d8e83
いまにむ <109068169+1m4nim@users.noreply.github.com>
2025-10-26 17:29:37 +0900

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
+// src/main.tsx
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+// -----------------------------------------------------------------
+// 🚨 Leafletの必須修正コード
+// -----------------------------------------------------------------
 import L from 'leaflet';
-import 'leaflet/dist/leaflet.css'; 
+import 'leaflet/dist/leaflet.css'; // 必須: 地図タイル表示のためのCSS
 delete L.Icon.Default.prototype._getIconUrl;
 
 L.Icon.Default.mergeOptions({
@@ -12,6 +17,7 @@ L.Icon.Default.mergeOptions({
   iconUrl: 'leaflet/dist/images/marker-icon.png',
   shadowUrl: 'leaflet/dist/images/marker-shadow.png',
 });
+// -----------------------------------------------------------------
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
