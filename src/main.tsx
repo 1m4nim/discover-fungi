9c54ed9a3c64f19058dffbc8869bcc131398f8da
いまにむ <109068169+1m4nim@users.noreply.github.com>
2025-10-26 16:30:56 +0900

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css'; 
 delete L.Icon.Default.prototype._getIconUrl;
