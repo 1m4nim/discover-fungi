5ce3d6539281dbf7eb28ba006510459c2d8b99ad
いまにむ <109068169+1m4nim@users.noreply.github.com>
2025-10-26 16:21:28 +0900

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -18,3 +18,11 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <App />
   </React.StrictMode>,
 );
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'leaflet/dist/images/marker-icon-2x.png',
+  iconUrl: 'leaflet/dist/images/marker-icon.png',
+  shadowUrl: 'leaflet/dist/images/marker-shadow.png',
+});
