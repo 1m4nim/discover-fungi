8f98e620366de86150fa56c3bf1d699d0015df4b
いまにむ <109068169+1m4nim@users.noreply.github.com>
2025-10-26 16:09:12 +0900

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,10 @@
-// src/main.tsx の理想的な構造
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx'; // ★Appコンポーネントが正しくインポートされているか確認
+import App from './App.tsx';
 import './index.css';
 
-// ----------------------------------------------------
-// Leafletの修正コードをここで実行しているはず
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
-// ... デフォルトアイコンの修正コード ...
 delete L.Icon.Default.prototype._getIconUrl;
 
 L.Icon.Default.mergeOptions({
@@ -17,11 +12,9 @@ L.Icon.Default.mergeOptions({
   iconUrl: 'leaflet/dist/images/marker-icon.png',
   shadowUrl: 'leaflet/dist/images/marker-shadow.png',
 });
-// ----------------------------------------------------
 
-// HTMLの #root 要素を取得し、そこにAppコンポーネントをレンダリング
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App /> {/* ★Appコンポーネントがここで呼び出されているか確認 */}
+    <App />
   </React.StrictMode>,
 );
