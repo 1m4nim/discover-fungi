cfa1277b5cf6927197117b6c51ca291727f2607a
いまにむ <109068169+1m4nim@users.noreply.github.com>
2025-10-26 16:29:41 +0900

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
+// -----------------------------------------------------------------
+// 🚨 Leafletの必須修正コード
+// -----------------------------------------------------------------
 import L from 'leaflet';
-import 'leaflet/dist/leaflet.css';
+import 'leaflet/dist/leaflet.css'; // 🚨 必須: 地図タイル表示のためのCSS
 delete L.Icon.Default.prototype._getIconUrl;
 
 L.Icon.Default.mergeOptions({
@@ -12,17 +15,10 @@ L.Icon.Default.mergeOptions({
   iconUrl: 'leaflet/dist/images/marker-icon.png',
   shadowUrl: 'leaflet/dist/images/marker-shadow.png',
 });
+// -----------------------------------------------------------------
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 );
-
-delete L.Icon.Default.prototype._getIconUrl;
-
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: 'leaflet/dist/images/marker-icon-2x.png',
-  iconUrl: 'leaflet/dist/images/marker-icon.png',
-  shadowUrl: 'leaflet/dist/images/marker-shadow.png',
-});
