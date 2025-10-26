8210edeab4493a8940ab496df83410cb2ec4a502
いまにむ <109068169+1m4nim@users.noreply.github.com>
2025-10-26 16:03:41 +0900

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,6 @@
 import L from 'leaflet';
-// CSSのインポートはここで行ってもOK
 import 'leaflet/dist/leaflet.css';
 
-// -----------------------------------------------------------------
-// 💡 マーカーアイコンの画像パス問題を解決するためのコード
-// -----------------------------------------------------------------
 delete L.Icon.Default.prototype._getIconUrl;
 
 L.Icon.Default.mergeOptions({
@@ -12,4 +8,3 @@ L.Icon.Default.mergeOptions({
   iconUrl: 'leaflet/dist/images/marker-icon.png',
   shadowUrl: 'leaflet/dist/images/marker-shadow.png',
 });
-// -----------------------------------------------------------------
