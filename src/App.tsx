65591d936c90bbf2955c75e23e338ba5c7817cd7
いまにむ <109068169+1m4nim@users.noreply.github.com>
2025-10-26 15:59:22 +0900

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import 'leaflet/dist/leaflet.css';
+import { MapContainer, TileLayer } from 'react-leaflet';
 
 function App() {
   const [count, setCount] = useState(0)
